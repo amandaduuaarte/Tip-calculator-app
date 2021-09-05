5b31737f6423c9bad06aec842bd50a78cfbcc9a8
Amanda Duarte <amandaduuaarte@gmail.com>
2021-09-05 15:31:22 -0300

    odifications in script.js

--- Assets/Js/script.js
+++ Assets/Js/script.js
@@ -1,48 +1,45 @@
 let bill = document.getElementById('bill')
-let tip;
-tip = document.getElementsByTagName('button')
+let tip = document.getElementsByName('tipValue')
+let numberOfPeople = document.getElementById('people')
+let tipAmountNumber = document.getElementById('tipPeople')
+let customTip = document.getElementById('tip')
+let operation
 
-
-//PENSAR EM UM JEITO MELHOR DE FAZER :T
-tip[0].addEventListener('click',()=>{
-    let tipType = tip[0].value
-    var operation = bill.value * (tipType/100)
-     operation = operation.toFixed(2)
-    alert(operation)
-  });
-tip[1].addEventListener('click',()=>{
-    let tipType = tip[1].value
-  var operation = bill.value * (tipType/100)
-   operation = operation.toFixed(2)
+function amount(arrayTip){
+  let tipType = arrayTip.value
+  operation = bill.value * (tipType/100)
+  operation = operation.toFixed(2)  
+  let calculated = numberOfPeople.value != 0 && numberOfPeople.value >1
+  
+    if (calculated) {
+  let tipForPerson = (operation)/numberOfPeople.value
+  tipAmountNumber.innerHTML = operation 
+  }else{
   alert(operation)
+  }
+}
+
+tip[0].addEventListener('click',()=>{
+    amount(tip[0])
+  });
+  
+tip[1].addEventListener('click',()=>{
+    amount(tip[1])
 });
 tip[2].addEventListener('click',()=>{
-    let tipType = tip[0].value
-    var operation = bill.value * (tipType/100)
-     operation = operation.toFixed(2)
-    alert(operation)
+    amount(tip[2])
   });
 tip[3].addEventListener('click',()=>{
-    let tipType = tip[1].value
-  var operation = bill.value * (tipType/100)
-   operation = operation.toFixed(2)
-  alert(operation)
+   amount(tip[3])
 });
 tip[4].addEventListener('click',()=>{
-    let tipType = tip[1].value
-  var operation = bill.value * (tipType/100)
-   operation = operation.toFixed(2)
-  alert(operation)
+    amount(tip[4])
 });
 
 // -------------- Custom ---------------
-
-let customTip = document.getElementById('tip')
 customTip.addEventListener('focusout', () =>{
     type = parseInt(customTip.value);
-    var operation = bill.value * (type/100)
+     operation = bill.value * (type/100)
     operation = operation.toFixed(2)
-    alert(operation)
+    //tipAmount(operation)
 })
-
-// ---------------- Number of people
